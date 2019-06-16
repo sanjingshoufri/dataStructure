bf3a675f8be55ff1fa53ef1bf8a4d329cdf65782
san3er <2303386665@qq.com>
2019-06-16 13:44:37 +0800

    linked list

---
+++ List/Singly-Linked.c
@@ -0,0 +1,17 @@
+/**
+ * 单链表.
+ */
+struct Node{
+    int element;   //链表基本单位的元素
+    int * pointer;
+};
+
+int lCount = 6;  //指定链表的长度
+
+
+int main(int argc, char const *argv[])
+{
+
+	/* code */
+	return 0;
+}
